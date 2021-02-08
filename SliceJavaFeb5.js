2ae8bb9a821035370687d4b8540258c15914db76
Kevin Martinez Palacios <69471354+kevinm777@users.noreply.github.com>
2021-02-08 16:01:25 -0600

    Add files via upload

---
+++ SliceJavaFeb5.js
@@ -0,0 +1,3 @@
+var sports=["Soccer","Baseball","Football","Swimming","Tennis"]
+var better_sports= sports.slice(2,5); // removes soccer and baseball and leaves football,swimming,and tennis
+console.log(better_sports);
---
+++ SpliceJavaFeb5.js
@@ -0,0 +1,4 @@
+var fruits=["apple","orange","dragonfruits","grapes"];
+fruits.splice(2,0,"apricots" , "banana" , "Avocado") //add three fruits
+fruits.splice(2,1); //remove dragonfruit
+console.log(fruits);
